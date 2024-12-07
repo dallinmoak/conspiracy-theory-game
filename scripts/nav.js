3dd4696960e6680c1fdc40d890f7168cb80d70c0
Brenleigh Killpack <kil21008@byui.edu>
2024-12-07 15:51:39 -0700

    fix: added js to correct paths based on location

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,21 +1,24 @@
 export function buildNav() {
+  const basePath = window.location.pathname.includes("personal-pages")
+    ? "../"
+    : "./";
   const navHTML = `
-    <header class="navbar" id="top">
-        <nav>
-            <div class="logo">
-                <a href="/index.html">
-                    <img src="/images/logo.png" alt="logo">
-                </a>
-            </div>
-            <div class="nav-links">
-                <a href="/personal-pages/alayna.html">Birds Aren't Real</a>
-                <a href="/personal-pages/brenleigh.html">The Moon Landing</a>
-                <a href="/personal-pages/dallin.html">Dallin's Page</a>
-                <a href="/personal-pages/peter.html">The Satanic Panic</a>
-                <a href="/personal-pages/quiz.html">Quiz</a>
-            </div>
-        </nav>
-    </header>
-  `;
+        <header class="navbar" id="top">
+            <nav>
+                <div class="logo">
+                    <a href="${basePath}index.html">
+                        <img src="${basePath}images/logo.png" alt="logo">
+                    </a>
+                </div>
+                <div class="nav-links">
+                    <a href="${basePath}personal-pages/alayna.html">Birds Aren't Real</a>
+                    <a href="${basePath}personal-pages/brenleigh.html">The Moon Landing</a>
+                    <a href="${basePath}personal-pages/dallin.html">Dallin's Page</a>
+                    <a href="${basePath}personal-pages/peter.html">The Satanic Panic</a>
+                    <a href="${basePath}personal-pages/quiz.html">Quiz</a>
+                </div>
+            </nav>
+        </header>
+    `;
   document.getElementById("nav").innerHTML = navHTML;
 }
